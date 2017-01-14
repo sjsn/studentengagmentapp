e24ecbc4c6f66ad681dbf535590d5d393b64720a
Samuel San Nicolas <samuelsan3@gmail.com>
2017-01-14 15:35:57 -0800

    Add test functions

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/api/question', methods=['GET'])
 def main():
-    return render_template('index.html')
+    if request.form['query'] is not None:
+        query = request.form['query']
+        answer = get(query)
+        result = {"query": query, "answer": answer}
+    return jsonify(result)
+
+def getInattentive(emotions):
+    // Get innattentive
 
 if __name__ == '__main__':
     app.run()
