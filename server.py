6604d620bf59547506285c5ae668789710ca407b
Magena <mfura@uw.edu>
2017-01-14 18:48:08 -0800

    changed gitignore

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import requests, json
-from secret import emotions_key
+from secrets import emotions_key
 
 app = Flask(__name__)
 
@@ -8,7 +8,6 @@ ms_emotion_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
 
 test_img = 'https://portalstoragewuprod2.azureedge.net/media/Default/Documentation/Computer-vision/Images/woman_roof.jpg'
 #'https://larrycuban.files.wordpress.com/2015/11/enhanced-buzz-wide-4644-1444018953-9.jpg'
-
 
 @app.route('/api/question', methods=['GET'])
 def main():
@@ -26,19 +25,18 @@ def getEmotions():
     headers['Ocp-Apim-Subscription-Key'] = emotions_key
     headers['Content-Type'] = 'application/json'
     data = None
-    emotion_list = response.json()
     response = requests.request('POST', ms_emotion_url, json=params, data=None, headers=headers, params=None)
     emotion_list = response.json()
     pretty = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ':'))
     print(emotion_list)
     print (pretty)
+    print(emotion_list())
 
 
-def getInattentive(emotion_list):
+def getInattentive(emotions): # I think emotions is a dictionary?
     # Get innattentive
-    if emotion_list[0]['score']['neutral'] < 0.7:
-        print "You are innattentive"
-    return getEmotions
+    return emotions # not this
+
 
 
 # if __name__ == '__main__':
@@ -46,8 +44,5 @@ def getInattentive(emotion_list):
 
 
 getEmotions()
-
-getInattentive('neutral')
-
 #emotions = getEmotions()
 # getInattentive(emotions) is this what we want??
