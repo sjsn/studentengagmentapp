9a99a89f28136eaefc1bd0e54c1465dc5ac2ad15
Samuel San Nicolas <samuelsan3@gmail.com>
2017-01-14 16:39:09 -0800

    Add getemotion base code

--- server.py
+++ server.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template, request, jsonify
+import requests, json
+from secrets import emotions_key
 
 app = Flask(__name__)
+
+ms_emotion_url = 'https://api.projectoxford.ai/emotion/v1.0/recognize'
+
+test_img = 'https://larrycuban.files.wordpress.com/2015/11/enhanced-buzz-wide-4644-1444018953-9.jpg'
 
 @app.route('/api/question', methods=['GET'])
 def main():
@@ -10,8 +16,24 @@ def main():
         result = {"query": query, "answer": answer}
     return jsonify(result)
 
-def getInattentive(emotions):
-    // Get innattentive
+def getEmotions():
+    params = {
+        "url": test_img
+    }
+    headers = {}
+    headers['Ocp-Apim-Subscription-Key'] = emotions_key
+    headers['Content-Type'] = 'application/json'
+    data = None
+    response = requests.request('POST', ms_emotion_url, json=params, data=None, headers=headers, params=None)
+    emotion_list = json.dumps(response.json(), sort_keys=True, indent=4, separators=(',', ':'))
+    print(emotion_list)
 
-if __name__ == '__main__':
-    app.run()
+def getInattentive(emotions):
+    # Get innattentive
+    return emotions
+
+
+# if __name__ == '__main__':
+#     app.run()
+
+getEmotions()
